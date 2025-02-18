6f8b4ea254c70f0e2c01a83ea14bba3ffc3cc242
Jassim Hayat <jassim.hayat@gmail.com>
2025-02-18 19:33:41 +0300

    add image

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router';
 import { signUp } from '../../services/authService'
+import SignUpImg from "../../assets/images/SignUp.png"
 
 import { UserContext } from '../../contexts/UserContext'
 
@@ -40,6 +41,7 @@ const SignUpForm = () => {
   return (
     <main>
       <h1>Sign Up</h1>
+      <img src={SignUpImg} />
       <p>{message}</p>
       <form onSubmit={handleSubmit}>
         <div>
