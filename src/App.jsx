7145eea0aa80a795e82d73dac79d0e72fec3d979
Jassim Hayat <jassim.hayat@gmail.com>
2025-02-19 20:09:07 +0300

    Add the NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,47 @@
-import { Routes, Route, useLocation } from 'react-router';
-import { useContext, useEffect } from 'react';
-import NavBar from './components/NavBar/NavBar';
-import SignUpForm from './components/SignUpForm/SignUpForm';
-import Landing from './components/Landing/Landing';
-import SignInForm from './components/SignInForm/SignInForm';
-import Dashboard from './components/Dashboard/Dashboard';
-import { UserContext } from './contexts/UserContext';
+import { Routes, Route, useLocation } from 'react-router'
+import { useContext, useEffect } from 'react'
+import NavBar from './components/NavBar/NavBar'
+import SignUpForm from './components/SignUpForm/SignUpForm'
+import Landing from './components/Landing/Landing'
+import SignInForm from './components/SignInForm/SignInForm'
+import Dashboard from './components/Dashboard/Dashboard'
+
+import { UserContext } from './contexts/UserContext'
 import './App.css';
 
 const App = () => {
-  const { user } = useContext(UserContext);
+
+  const { user } = useContext(UserContext)
   const location = useLocation();
 
   useEffect(() => {
     const body = document.body;
 
-    // Remove previous classes
-    body.classList.remove('landing-bg', 'signin-bg', 'signup-bg');
+    //remove previous classes
+    body.classList.remove('landing-bg', 'signin-bg', 'singup-bg');
 
-    // Add the class based on the current route
+    //add the class based on the current route
     if (location.pathname === '/') {
       body.classList.add('landing-bg');
     } else if (location.pathname === '/sign-in') {
-      body.classList.add('signin-bg');
+      body.classList.add('signin-bg')
     } else if (location.pathname === '/sign-up') {
-      body.classList.add('signup-bg');
+      body.classList.add('signup-bg')
     }
+
   }, [location.pathname]);
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
-        <Route path="/sign-in" element={<SignInForm />} />
-        <Route path="/sign-up" element={<SignUpForm />} />
+        {/* if user exists show dashboard otherwise show landing page */}
+        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path='/sign-up' element={<SignUpForm />} />
       </Routes>
     </>
   );
 };
 
-export default App;
+export default App
