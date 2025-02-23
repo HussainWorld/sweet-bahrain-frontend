41c73a066482708677c91816f92d0a439ad25920
HussainWorld <hussain.university.poly@gmail.com>
2025-02-23 19:44:51 +0300

    dynamic admin navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,42 +4,65 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { UserContext } from '../../contexts/UserContext'
 
 const NavBar = () => {
-    const { user, setUser } = useContext(UserContext);
+	const { user, setUser } = useContext(UserContext);
 
-    const handleSignOut = () => {
-        localStorage.removeItem('token');
-        setUser(null);
-    };
+	let isAdmin
+	if (user) {
+		const userType = user.userType;
+		isAdmin = userType == 'admin'
+	}
 
-    return (
-        <Navbar bg="light" variant="light" expand="lg" fixed="top"> {/* Fixed at top */}
-            <Container>
-                <Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        {user ? (
-                            <>
-                                <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-                                <Nav.Link as={Link} to="/" onClick={handleSignOut}>
-                                    Sign Out
-                                </Nav.Link>
-                                <Navbar.Text className="ms-3">
-                                    Welcome, {user.username}
-                                </Navbar.Text>
-                            </>
-                        ) : (
-                            <>
-                                <Nav.Link as={Link} to="/sign-up">Sign Up</Nav.Link>
-                                <Nav.Link as={Link} to="/sign-in">Sign In</Nav.Link>
-                                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+	const handleSignOut = () => {
+		localStorage.removeItem('token');
+		setUser(null);
+	};
+
+	return (
+		<Navbar bg="light" variant="light" expand="lg" fixed="top"> {/* Fixed at top */}
+			<Container>
+				<Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						{user ? (
+							<>
+
+								{isAdmin ? (
+									<>
+										<Navbar.Text className="ms-3">
+											Welcome Admin, {user.username}
+										</Navbar.Text>
+								<Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+								<Nav.Link as={Link} to="/" onClick={handleSignOut}>
+									Sign Out
+								</Nav.Link>
+									</>
+								) : (
+									<>
+									<Navbar.Text className="ms-3">
+									Welcome, {user.username}
+								</Navbar.Text>					
+								<Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+								<Nav.Link as={Link} to="/" onClick={handleSignOut}>
+									Sign Out
+								</Nav.Link>
+								</>
+					)}
+							</>
+						) : (
+							<>
+								<Nav.Link as={Link} to="/sign-up">Sign Up</Nav.Link>
+								<Nav.Link as={Link} to="/sign-in">Sign In</Nav.Link>
+								<Nav.Link as={Link} to="/">Home</Nav.Link>
+							</>
+						)}
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 };
 
 export default NavBar;
+
+
